75086f4b07105a3112cd2853b1ca099f1e5961f1
PioV1996 <gustavo_app@outlook.com>
2020-10-19 22:58:53 -0500

    LogIn 1

--- app.rb
+++ app.rb
@@ -1,7 +1,33 @@
+current_dir = Dir.pwd
+Dir["#{current_dir}/models/*.rb"].each {|file| require file}
+require 'active_record'
+require 'sinatra/activerecord'
 require 'sinatra'
 
 set :bind, '0.0.0.0'
 
+$empleados = Array.new
+$consulta = Users.all
+$consulta.each do |r|
+    $empleados.push(r['usuario'], r['pass'])
+end
+
 get '/' do
     erb :index
+end
+
+post '/login' do
+    @usuario = params['usr_name']
+    @pass    = params['pass']
+
+    user = $empleados.include?(@usuario)
+    pass = $empleados.include?(@pass)
+    if user == true && pass == false
+        erb :pass_fail
+    elsif user == false
+        erb :no_user
+    else
+        @llamadas = Call.all
+        erb :calls
+    end
 end
